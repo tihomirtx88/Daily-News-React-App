c5de248a030881483dd3c773d190a616e96757ba
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2023-03-04 08:49:41 +0200

    Add post by id logic

--- src/compontents/posts/index.js
+++ src/compontents/posts/index.js
@@ -1,9 +1,45 @@
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import Moment from "react-moment";
+import { useEffect } from "react";
+import { fetchPost } from "../../store/utils/thinks";
+import { Spinner } from 'react-bootstrap';
+
 const Posts = () => {
-    return(
-        <>
-          Contact
-        </>
-    );
-}
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+  let params = useParams();
+
+  useEffect(() => {
+    dispatch(fetchPost(params.id));
+  }, []);
+
+  return (
+    <>
+      {posts.postById ? (
+        <div className="article_container">
+          <h1>{posts.postById.title}</h1>
+          <div
+            className="image"
+            style={{ background: `url(${posts.postById.imagexl})` }}
+          ></div>
+          <div className="author">
+            Created by <span>{posts.postById.author} - </span>
+            <Moment format="DD MMMM">{posts.postById.createdAt}</Moment>
+          </div>
+          <div className="mt-3 content">{posts.postById.content}</div>
+        </div>
+      ) : null}
+
+      {posts.loading ? (
+        <div style={{ textAlign: "center" }}>
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      ) : null}
+    </>
+  );
+};
 
 export default Posts;
